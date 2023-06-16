5469351866031591249e0cf2c01a4a86611b8f06
Matt <matthewdavid99@comcast.net>
2023-06-16 12:38:04 -0400

    test

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-
+# hello
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
