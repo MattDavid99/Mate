f5fa893ff56aca089b1df6e5cba8b66292a1f2ef
Matt <matthewdavid99@comcast.net>
2023-06-21 19:41:35 -0400

    started match.js redux

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import match from "./match"
 
 const rootReducer = combineReducers({
   session,
+  match
 });
 
 
---
+++ react-app/src/store/match.js
@@ -0,0 +1,98 @@
+// constants
+const CREATE_MATCH = "match/CREATE_MATCH";
+const MAKE_MOVES = "match/MAKE_MOVES"
+
+
+const startMatch = (match) => {
+  return {
+    type: CREATE_MATCH,
+    payload: match,
+  }
+};
+
+const makeMoves = (match) => {
+  return {
+    type: MAKE_MOVES,
+    payload: match
+  }
+}
+
+
+
+// @match_routes.route('/', methods=['POST'])
+export const createMatch = (white_player_id, black_player_id) => async (dispatch) => {
+  const response = await fetch('/api/match/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      white_player_id,
+      black_player_id
+    })
+  })
+
+  if (response.ok) {
+    const data = await response.json()
+    dispatch(startMatch(data.match[0])) // <<-- might just be data.match
+
+  } else if (response.status < 500) {
+    const data = await response.json()
+
+    if (data.errors) {
+      return data.errors
+    }
+
+  }
+};
+
+
+// @match_routes.route('/<int:match_id>/move', methods=['POST'])
+export const postMove = (match_id, uci_move) => async (dispatch) => {
+  const response = await fetch(`/api/match/${match_id}/move`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      move: uci_move
+    })
+  })
+
+  if (response.ok) {
+    const data = await response.json()
+    dispatch(makeMoves(data.match[0]))
+
+  } else if (response.status < 500) {
+    const data = await response.json()
+
+    if (data.errors) {
+      return data.errors
+    }
+  }
+}
+
+
+
+
+const initialState = { match: null };
+
+export default function matchReducer(state = initialState, action) {
+	switch (action.type) {
+
+    case CREATE_MATCH:
+      return {
+        ...state,
+        match: action.payload
+      }
+
+    case MAKE_MOVES:
+      return {
+        ...state,
+        match: action.payload
+      }
+
+		default:
+			return state;
+	}
+}
