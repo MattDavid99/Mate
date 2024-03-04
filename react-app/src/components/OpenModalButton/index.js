8832f59a0c5346c0711d4c8915002a2cd9a0c4d8
Matthew David <102698225+MattDavid99@users.noreply.github.com>
2024-03-04 16:42:11 -0500

    Update index.js

--- react-app/src/components/OpenModalButton/index.js
+++ react-app/src/components/OpenModalButton/index.js
@@ -5,8 +5,8 @@ import "./OpenModalButton.css"
 function OpenModalButton({
   modalComponent, 
   buttonText, 
-  onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
-  onModalClose // optional: callback function that will be called once the modal is closed
+  onButtonClick, 
+  onModalClose
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 
