2f5dd649af629f96df66cf57372dad9bf20e62db
Matthew David <102698225+MattDavid99@users.noreply.github.com>
2024-03-15 16:48:51 -0400

    Update Modal.js

--- react-app/src/context/Modal.js
+++ react-app/src/context/Modal.js
@@ -21,8 +21,8 @@ export function ModalProvider({ children }) {
     modalRef, 
     modalContent, 
     setModalContent, 
-    setOnModalClose, // function to set the callback function called when modal is closing
-    closeModal // function to close the modal
+    setOnModalClose, 
+    closeModal 
   };
 
   return (
@@ -37,11 +37,8 @@ export function ModalProvider({ children }) {
 
 export function Modal() {
   const { modalRef, modalContent, closeModal } = useContext(ModalContext);
-  // If there is no div referenced by the modalRef or modalContent is not a
-  // truthy value, render nothing:
   if (!modalRef || !modalRef.current || !modalContent) return null;
 
-  // Render the following component to the div referenced by the modalRef
   return ReactDOM.createPortal(
     <div id="modal">
       <div id="modal-background" onClick={closeModal} />
