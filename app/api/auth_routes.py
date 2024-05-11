c029c5accbf7631ad18fe54b5bc8d68dc39f89af
Matthew David <102698225+MattDavid99@users.noreply.github.com>
2024-05-10 23:43:51 -0400

    Update auth_routes.py

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -70,7 +70,6 @@ def sign_up():
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
-
 @auth_routes.route('/unauthorized')
 def unauthorized():
     """
