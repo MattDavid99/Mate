80b73f84dcd2d7f0e70fcd47a1452412d6364f55
Matthew David <102698225+MattDavid99@users.noreply.github.com>
2024-05-24 23:50:18 -0400

    Update login_form.py

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -12,7 +12,6 @@ def user_exists(form, field):
 
 
 def password_matches(form, field):
-    # Checking if password matches
     password = field.data
     email = form.data['email']
     user = User.query.filter(User.email == email).first()
