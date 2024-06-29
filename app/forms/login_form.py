fe9821e8bf883116defe1379967ff79509dc73aa
Matthew David <102698225+MattDavid99@users.noreply.github.com>
2024-06-29 16:07:57 -0400

    Update login_form.py

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -19,7 +19,6 @@ def password_matches(form, field):
     if not user.check_password(password):
         raise ValidationError('Password was incorrect.')
 
-
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired(), password_matches])
