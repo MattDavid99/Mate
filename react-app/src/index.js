26c543c0dec02a6576bbf3efb392ed20e3570db7
Matthew David <102698225+MattDavid99@users.noreply.github.com>
2024-10-19 12:59:35 -0400

    Update index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
 import { ModalProvider, Modal } from "./context/Modal";
 import configureStore from "./store";
 import * as sessionActions from "./store/session";
